02c64a32729edf8e2f9f3f24aceb0121aa439bda
Denito37 <69406178+Denito37@users.noreply.github.com>
2023-04-21 14:54:28 -0400

    set date on client side

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,14 @@
-
+import { useState, useEffect } from "react";
 export default function Header(){
-    const queryFilter = ['text=animals','text=cats','text=dogs','text=frog','text=bird','text=fruit','text=vegetable','text=curry','entities=PER:Lebron James','entities=PER:Jack Black','entities=LOC:Hawaii','entities=ORG:Lego','news-sources=https://www.positive.news']
+    const [theDate, setTheDate] = useState('')
     const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
     const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
     const date = new Date();
     const fullDate = `${weekday[date.getDay()]}, ${month[date.getMonth()]} ${date.getDate()} ${date.getFullYear()}`
+
+    useEffect(() =>{
+        setTheDate(fullDate)
+    },[fullDate])
 
     return(
         <header className=" w-full">
@@ -13,7 +17,7 @@ export default function Header(){
             </h1>
             <hr className=" my-2 h-[2px] bg-red-800" />
             <h2 className=" text-center text-xl">
-                {fullDate}
+                {theDate}
             </h2>
             <hr className=" my-2 h-[2px] bg-red-800" />
         </header>
